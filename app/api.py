cecc254ce3e444ad240e8f5c99bd580209f99606
adarsh <adarshsahu2510@gmail.com>
2025-05-29 15:13:29 +0530

    .

--- app/api.py
+++ app/api.py
@@ -1,143 +1,176 @@
+# instantid_api.py
+
 import io
+import os
+import cv2
+import math
 import base64
 import torch
+import random
 import numpy as np
 from PIL import Image
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Optional
-from diffusers import EulerDiscreteScheduler, LCMScheduler
-from diffusers.utils import load_image
-import cv2
-import random
-
-from insightface.app import FaceAnalysis
+from typing import Optional, List, Union
+from model_util import load_models_xl, get_torch_device
 from pipeline_stable_diffusion_xl_instantid_full import StableDiffusionXLInstantIDPipeline
-from controlnet_aux import MidasDetector
-from diffusers.models import ControlNetModel
+from controlnet_util import openpose, get_depth_map, get_canny_image
+from diffusers import ControlNetModel, EulerDiscreteScheduler, LCMScheduler
+from diffusers.pipelines.controlnet.multicontrolnet import MultiControlNetModel
+from diffusers.utils import load_image
+from insightface.app import FaceAnalysis
+from style_template import styles
 
 # Setup
 app = FastAPI()
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+MAX_SEED = np.iinfo(np.int32).max
+DEFAULT_STYLE_NAME = "Watercolor"
+device = get_torch_device()
+dtype = torch.float16 if "cuda" in str(device) else torch.float32
+STYLE_NAMES = list(styles.keys())
 
-# Load InsightFace
-face_analyzer = FaceAnalysis(name='antelopev2', root='./', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+# Load Face Analyzer
+face_analyzer = FaceAnalysis(name="antelopev2", root="./", providers=["CUDAExecutionProvider", "CPUExecutionProvider"])
 face_analyzer.prepare(ctx_id=0, det_size=(640, 640))
 
-# Load ControlNet and pipeline
-controlnet = ControlNetModel.from_pretrained("./checkpoints/ControlNetModel", torch_dtype=dtype)
+# Load ControlNets
+controlnet_identitynet = ControlNetModel.from_pretrained("./checkpoints/ControlNetModel", torch_dtype=dtype)
+controlnet_pose = ControlNetModel.from_pretrained("thibaud/controlnet-openpose-sdxl-1.0", torch_dtype=dtype).to(device)
+controlnet_canny = ControlNetModel.from_pretrained("diffusers/controlnet-canny-sdxl-1.0", torch_dtype=dtype).to(device)
+controlnet_depth = ControlNetModel.from_pretrained("diffusers/controlnet-depth-sdxl-1.0-small", torch_dtype=dtype).to(device)
+
+controlnet_map = {"pose": controlnet_pose, "canny": controlnet_canny, "depth": controlnet_depth}
+controlnet_map_fn = {"pose": openpose, "canny": get_canny_image, "depth": get_depth_map}
+
+# Load Pipeline
 pipe = StableDiffusionXLInstantIDPipeline.from_pretrained(
     "wangqixun/YamerMIX_v8",
-    controlnet=controlnet,
+    controlnet=[controlnet_identitynet],
     torch_dtype=dtype,
     safety_checker=None,
-    feature_extractor=None,
+    feature_extractor=None
 ).to(device)
 pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
 pipe.load_ip_adapter_instantid("./checkpoints/ip-adapter.bin")
 pipe.load_lora_weights("latent-consistency/lcm-lora-sdxl")
 pipe.disable_lora()
 
-# Midas for depth (optional)
-midas = MidasDetector.from_pretrained("lllyasviel/Annotators")
+# Utils
+def decode_image(input_data: str) -> Image.Image:
+    if input_data.startswith("http://") or input_data.startswith("https://"):
+        return load_image(input_data)
+    return Image.open(io.BytesIO(base64.b64decode(input_data)))
 
-# Utility functions
-def decode_base64_image(base64_str):
-    return Image.open(io.BytesIO(base64.b64decode(base64_str)))
-
-def resize_img(input_image, max_side=1280, min_side=1024, base_pixel_number=64):
+def resize_img(input_image: Image.Image, max_side=1280, min_side=1024) -> Image.Image:
     w, h = input_image.size
     ratio = min_side / min(h, w)
     w, h = round(ratio * w), round(ratio * h)
     ratio = max_side / max(h, w)
-    w, h = round(ratio * w), round(ratio * h)
-    w = (w // base_pixel_number) * base_pixel_number
-    h = (h // base_pixel_number) * base_pixel_number
-    return input_image.resize([w, h], Image.BILINEAR)
+    input_image = input_image.resize([round(ratio * w), round(ratio * h)], Image.BILINEAR)
+    return input_image
 
-def convert_pil_to_cv2(img: Image.Image):
+def convert_to_cv2(img: Image.Image):
     return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
-def draw_kps(image_pil, kps):
+def draw_kps(img: Image.Image, kps):
     from pipeline_stable_diffusion_xl_instantid_full import draw_kps
-    return draw_kps(image_pil, kps)
+    return draw_kps(img, kps)
 
-# Request model
-class InferenceInput(BaseModel):
+def apply_style(style_name, positive, negative=""):
+    p, n = styles.get(style_name, styles[DEFAULT_STYLE_NAME])
+    return p.replace("{prompt}", positive), n + " " + negative
+
+# Request Model
+class BlendInput(BaseModel):
     face_image: str
     pose_image: Optional[str] = None
     prompt: str
-    negative_prompt: Optional[str] = "(lowres, low quality, worst quality:1.2), (text:1.2), watermark, (frame:1.2), deformed, ugly, deformed eyes, blur, out of focus, blurry, deformed cat, deformed, photo, anthropomorphic cat, monochrome, pet collar, gun, weapon, blue, 3d, drones, drone, buildings in background, green"
-    style_name: Optional[str] = "none"
+    negative_prompt: Optional[str] = ""
+    style_name: Optional[str] = DEFAULT_STYLE_NAME
     num_steps: Optional[int] = 30
     identitynet_strength_ratio: Optional[float] = 0.8
     adapter_strength_ratio: Optional[float] = 0.8
+    pose_strength: Optional[float] = 0.4
+    canny_strength: Optional[float] = 0.3
+    depth_strength: Optional[float] = 0.5
+    controlnet_selection: Optional[List[str]] = ["pose"]
     guidance_scale: Optional[float] = 5.0
     seed: Optional[int] = 42
+    scheduler: Optional[str] = "EulerDiscreteScheduler"
     randomize_seed: Optional[bool] = True
     enable_LCM: Optional[bool] = False
     enhance_face_region: Optional[bool] = True
 
 @app.post("/blend")
-def blend_face(data: InferenceInput):
+def blend_face(data: BlendInput):
     if data.randomize_seed:
-        data.seed = random.randint(0, 2**31 - 1)
+        data.seed = random.randint(0, MAX_SEED)
 
-    face_img = decode_base64_image(data.face_image)
-    face_img = resize_img(face_img)
-    face_cv2 = convert_pil_to_cv2(face_img)
+    prompt, negative_prompt = apply_style(data.style_name, data.prompt, data.negative_prompt)
+
+    face_image = resize_img(decode_image(data.face_image), max_side=1024)
+    face_cv2 = convert_to_cv2(face_image)
+    height, width, _ = face_cv2.shape
 
     face_info = face_analyzer.get(face_cv2)
     if not face_info:
-        raise HTTPException(status_code=400, detail="No face detected in source image.")
+        raise HTTPException(status_code=400, detail="No face detected in face_image")
 
-    face_info = sorted(face_info, key=lambda x: (x['bbox'][2] - x['bbox'][0]) * (x['bbox'][3] - x['bbox'][1]))[-1]
+    face_info = sorted(face_info, key=lambda x: (x['bbox'][2]-x['bbox'][0]) * (x['bbox'][3]-x['bbox'][1]))[-1]
     face_emb = face_info['embedding']
-    face_kps = draw_kps(face_img, face_info['kps'])
+    face_kps = draw_kps(face_image, face_info['kps'])
 
+    img_controlnet = face_image
     if data.pose_image:
-        pose_img = decode_base64_image(data.pose_image)
-        pose_img = resize_img(pose_img)
-        pose_cv2 = convert_pil_to_cv2(pose_img)
+        pose_image = resize_img(decode_image(data.pose_image), max_side=1024)
+        pose_cv2 = convert_to_cv2(pose_image)
         pose_info = face_analyzer.get(pose_cv2)
         if not pose_info:
-            raise HTTPException(status_code=400, detail="No face detected in pose image.")
-        pose_info = pose_info[-1]
-        face_kps = draw_kps(pose_img, pose_info['kps'])
-        face_kps = face_kps.resize(pose_img.size)  # Force same size
-        width, height = face_kps.size
-    else:
-        face_kps = face_kps.resize(face_img.size)  # Force same size
+            raise HTTPException(status_code=400, detail="No face detected in pose_image")
+        face_kps = draw_kps(pose_image, pose_info[-1]['kps'])
+        img_controlnet = pose_image
         width, height = face_kps.size
 
     control_mask = None
     if data.enhance_face_region:
-        control_mask_np = np.zeros([height, width, 3], dtype=np.uint8)
+        mask = np.zeros([height, width, 3])
         x1, y1, x2, y2 = map(int, face_info["bbox"])
-        control_mask_np[y1:y2, x1:x2] = 255
-        control_mask = Image.fromarray(control_mask_np)
-
-        if control_mask.size != face_kps.size:
-            control_mask = control_mask.resize(face_kps.size, resample=Image.BILINEAR)
+        mask[y1:y2, x1:x2] = 255
+        control_mask = Image.fromarray(mask.astype(np.uint8))
 
     if data.enable_LCM:
-        pipe.enable_lora()
         pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
+        pipe.enable_lora()
     else:
         pipe.disable_lora()
         pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
 
+    if data.controlnet_selection:
+        pipe.controlnet = MultiControlNetModel([
+            controlnet_identitynet
+        ] + [controlnet_map[k] for k in data.controlnet_selection])
+
+        control_scales = [float(data.identitynet_strength_ratio)] + [
+            {"pose": data.pose_strength, "canny": data.canny_strength, "depth": data.depth_strength}[k] for k in data.controlnet_selection
+        ]
+
+        control_images = [face_kps] + [
+            controlnet_map_fn[k](img_controlnet).resize((width, height)) for k in data.controlnet_selection
+        ]
+    else:
+        pipe.controlnet = controlnet_identitynet
+        control_scales = float(data.identitynet_strength_ratio)
+        control_images = face_kps
+
     pipe.set_ip_adapter_scale(data.adapter_strength_ratio)
     generator = torch.Generator(device=device).manual_seed(data.seed)
-
     result = pipe(
-        prompt=data.prompt,
-        negative_prompt=data.negative_prompt,
+        prompt=prompt,
+        negative_prompt=negative_prompt,
         image_embeds=face_emb,
-        image=face_kps,
+        image=control_images,
         control_mask=control_mask,
-        controlnet_conditioning_scale=float(data.identitynet_strength_ratio),
+        controlnet_conditioning_scale=control_scales,
         num_inference_steps=data.num_steps,
         guidance_scale=data.guidance_scale,
         height=height,
@@ -145,8 +178,7 @@ def blend_face(data: InferenceInput):
         generator=generator
     ).images[0]
 
-    buffered = io.BytesIO()
-    result.save(buffered, format="PNG")
-    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
-    return {"image": f"data:image/png;base64,{img_str}"}
-
+    buffer = io.BytesIO()
+    result.save(buffer, format="PNG")
+    img_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
+    return {"image": img_str}
